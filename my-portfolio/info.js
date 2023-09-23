a784e7d09c32d1b3d4810153c07c12e7391a0482
Csuka Balázs <csukabalazs1219@gmail.com>
2023-09-24 00:12:50 +0200

    Init

---
+++ my-portfolio/info.js
@@ -0,0 +1,66 @@
+let info = {
+    name: "Balázs Csuka",
+    logo_name: "Balázs Csuka",
+    flat_picture: require("./src/assets/portrait.png"),
+    config: {
+      use_cookies: true,
+      navbar: {
+        blur: false
+      }
+    },
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus semper nisi sit amet tincidunt. Aliquam erat volutpat. Donec luctus, nunc posuere malesuada facilisis, magna dolor pellentesque metus, eu sollicitudin ante augue nec erat. Nullam aliquam luctus nulla, nec porta purus dictum vitae. Integer vel rutrum turpis.",
+    education: [
+      {
+        name: "DSZC Beregszászi Pál",
+        place: "Debrecen/Hungary",
+        date: "Sep 2019 - May 2021",
+        degree: "OKJ, Computer Software Engineering",
+        skills: [
+        ]
+      },
+      {
+        name: "University of Debrecen",
+        place: "Hungary",
+        date: "Sep 2021 - Present",
+        degree: "Bachelor of Science, Computer Software Engineering",
+        description:
+          "",
+        skills: [
+          "C",
+         "Java",
+         "C#",
+         "Python",
+          "Matlab",
+          "Verilog",
+          "HTML5",
+          "CSS3",
+          "JavaScript",
+          "Sql",
+        ]
+      }
+    ],
+    experience: [
+  
+      {
+        name: "Software Developer",
+        place: "CTS-Informatika Kft, Hungary",
+        date: "Jun, 2021 - Present",
+        position: "Software Developer",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus finibus semper nisi sit amet tincidunt. Aliquam erat volutpat. Donec luctus, nunc posuere malesuada facilisis, magna dolor pellentesque metus, eu sollicitudin ante augue nec erat. Nullam aliquam luctus nulla, nec porta purus dictum vitae. Integer vel rutrum turpis.",
+          skills: [
+            "C#",
+            "Blazor",
+            "WinForms",
+            "Postgres",
+            ".NET",
+            "Maui",
+            "ASP.NET Web API"
+          ]
+      },
+  
+    ],
+  };
+  
+  export default info;
---
+++ my-portfolio/src/components/MyAbout.vue
@@ -0,0 +1,70 @@
+<template>
+  <div :class="{ 'bg-white': !nightMode, 'bg-dark': nightMode }" class="pt-5 p-st">
+    <div class="container"
+      data-aos="fade"
+      data-aos-once="true"
+      data-aos-duration="1000">
+      <div class="row">
+        <div class="col-lg-7 col-md-12 col-12 d-flex align-items-center">
+          <div class="about-text">
+            <h1 class="text-holder">
+              <span class="mr-2 welcome-text">Hey folks!</span>
+            </h1>
+            <p>{{description}}</p>
+            <div class="pb-4 col-md-10 mt-4">
+                <button v-for="socialLink in socialLinks" :key="socialLink"
+                class="btn btn-outline-secondary mx-2 "
+                @click="open(socialLink)"
+                v-tooltip.bottom="'socialLink'"
+                >
+                <i :class="['fab fa-' + socialLink]"></i>
+            </button>
+          </div>
+          </div>
+        </div>
+        <div class="col-lg-5 col-md-12 col-12">
+          <div class="about-image svg">
+            <img src="@/assets/portrait.png" class="img-fluid" style="border-radius: 50%;" alt="svg image">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </template>
+  
+  <script>
+  import info from "../../info";
+  export default {
+    props: {
+      nightMode: {
+        type: Boolean,
+      },
+    },
+    components:{
+    },
+    name: "MyAbout",
+    data() {
+      return {
+        animatedItems: ["Marvel Sann", "Web Designer", "UI Specialist"],
+        description: info.description,
+        socialLinks: ["linkedin", "github"]
+      };
+    },
+  };
+  </script>
+
+  <style scoped>
+    .welcome-text{
+        font-size: 1em;
+        font-weight: bold;
+    }
+    .text-holder{
+        margin: 24px 0;
+    }
+    @media (max-width: 991.5px) {
+        .row {
+            flex-direction: column-reverse; /* Change to column layout, with the right column on top */
+        }
+    }
+  </style>
+    
